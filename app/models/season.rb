455a9cad1ae5739354e735c62e724ce01e3457a9
Spencer Smitherman <spencer.smitherman@gmail.com>
2014-03-26 22:36:14 -0700

    Refactored model but still having issues with updating attributes

--- app/models/season.rb
+++ app/models/season.rb
@@ -11,31 +11,32 @@ class Season < ActiveRecord::Base
 	def self.find_urls
 		urls = []
 		Year.all.each do |year|
-			urls << "http://espn.go.com/nfl/standings/_/year/#{year.year}/group/1"
+			urls << Nokogiri.parse(HTTParty.get("http://espn.go.com/nfl/standings/_/year/#{year.year}/group/1"))
 		end
 		Season.parse_each_url(urls)
 	end
 
 	def self.parse_each_url(urls)
-		urls.each do |url|
-			doc = Nokogiri.parse(HTTParty.get(url))
-			Season.find_home_win_loss(doc)
+		Team.all.each do |team|
+			urls.each do |url|
+				Season.find_home_win_loss(url, team)
+			end
 		end
 	end
 
-	def self.find_home_win_loss(doc)
-		Team.all.each do |team|
-			if doc.search(".#{team.espn_id} a").inner_text == team.name
-				total_record = doc.search(".#{team.espn_id} td:nth-child(6)").inner_text
-				Season.save_home_record(total_record, team)
-			end
+	def self.find_home_win_loss(doc, team)
+		if doc.search(".#{team.espn_id} a").inner_text == team.name
+			total_record = doc.search(".#{team.espn_id} td:nth-child(6)").inner_text
+			Season.save_home_record(total_record, team)
 		end
 	end
 
 	def self.save_home_record(total_record, team)
 		wins = total_record.scan(/^\d{1,}/).join.to_i
 		losses = total_record.scan(/-(\d{1,})-/).join.to_i
-		Season.find(team.id).update_attributes(wins: wins, losses: losses)
+		@@counter = 1
+		Season.find(@@counter).update_attributes(wins: wins, losses: losses)
+		@@counter += 1
 	end
 
 end
