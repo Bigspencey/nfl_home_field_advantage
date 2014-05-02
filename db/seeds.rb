9b355dc9d7cb60e82b66443658cd925e1b499ddd
Spencer Smitherman <spencer.smitherman@gmail.com>
2014-05-02 11:05:07 -0700

    Moved CSV parsing over to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,14 +22,20 @@ espn = ["team-28-22", "team-28-1", "team-28-33", "team-28-2", "team-28-29", "tea
 				"team-28-15", "team-28-16", "team-28-17", "team-28-18", "team-28-19", "team-28-20", "team-28-13", "team-28-21",
 				"team-28-23", "team-28-24", "team-28-25", "team-28-26", "team-28-14", "team-28-27", "team-28-10", "team-28-28"]
 
-teams.each_with_index do |team, index|
-	Team.create(name: team, mascot: mascots[index], stadium_name: stadiums[index], espn_id: espn[index])
-end
-
 years = ["2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004"]
 
 start_date = ["2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005"]
 
 teams.each_with_index do |team, index|
 	Team.create(name: team, mascot: mascots[index], stadium_name: stadiums[index], espn_id: espn[index])
+end
+
+CSV.foreach("lib/assets/total_offense.csv") do |row|
+	if row[0] == Season.find_by_ && row[2] == Season.where(year: row[2])
+		Season.update_attributes(avg_offense: row[1])
+	end
+end
+
+CSV.foreach("lib/assets/total_defense.csv") do |row|
+	Season.update_attributes(avg_defense: row[1])
 end
