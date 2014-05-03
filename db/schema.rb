868c7d3f62fb742fd1f085f1706771b2cc9d4385
Spencer Smitherman <spencer.smitherman@gmail.com>
2014-05-02 17:05:29 -0700

    Deleted Year Migration - Everything works

--- db/schema.rb
+++ db/schema.rb
@@ -38,4 +38,11 @@ ActiveRecord::Schema.define(version: 20140326173915) do
     t.datetime "updated_at"
   end
 
+  create_table "years", id: false, force: true do |t|
+    t.string   "year"
+    t.string   "start_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
