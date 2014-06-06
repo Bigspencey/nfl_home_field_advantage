395af70393e4a36880b503df9f75b332eb35af4f
Spencer Smitherman <spencer.smitherman@gmail.com>
2014-06-06 14:46:38 -0700

    That didn't work

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-require 'rack/cache'
-
-config.middleware.delete Rack::Cache
-
-# if !Rails.env.development? && !Rails.env.test?
-#   config.middleware.insert_before Rack::Cache, Rack::Static, urls: [config.assets.prefix],     root: 'public'
-# end
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
